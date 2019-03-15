c183c40cf96f0128c1b5b221becb2d2a0e7509b1
Xabier C <xabier_abia@hotmail.com>
2019-03-15 01:38:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
   array.each do |num|
-    new_array = array ** 2
-  end  
+    new_array = array(-1)
+    new_array **2
+    new_array_2 = []
+    new.array_2.push(new array)
+    end 
+  return new_array_2.reverse
 end
